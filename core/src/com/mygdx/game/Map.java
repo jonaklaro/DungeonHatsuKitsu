94a4b6ea4a34ec8152cfff9ff62f98595840e12c
jonanarchy <jmallet0507@gmail.com>
2022-05-23 13:43:01 +0200

    larger Map

--- core/src/com/mygdx/game/Map.java
+++ core/src/com/mygdx/game/Map.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.math.Vector2;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Set;
 
 public class Map extends Sprite {
     BufferedReader br;
