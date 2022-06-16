00bc8fce5f1d0a41d67c277fa1d5d20bcb4b92a8
roggen <robin@seerig.de>
2022-06-16 19:48:09 +0200

    hier sind noch paar werte falsch

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,9 +9,14 @@ import java.awt.*;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-
+    config.setTitle("Praktikum");
+    config.setWindowedMode(1280, 720);
+    config.setResizable(false);
+    config.setIdleFPS(144);
+    config.useVsync(true);
+    config.useOpenGL3(true, 3, 2);
 		//Grundlegende Einstellungen (FPS, Icon, Bildgröße)
-		config.setForegroundFPS(60);
+		config.setForegroundFPS(144);
 		config.setWindowedMode(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height);
 		config.setWindowIcon("anderes.png");
 		new Lwjgl3Application(new Praktikum(), config);
