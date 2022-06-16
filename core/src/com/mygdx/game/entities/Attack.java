f3d0f9e74529367a05e44b7e84a0d1693efa797b
jonanarchy <jmallet0507@gmail.com>
2022-06-16 17:05:44 +0200

    enemie movement system

--- core/src/com/mygdx/game/entities/Attack.java
+++ core/src/com/mygdx/game/entities/Attack.java
@@ -9,21 +9,26 @@ public class Attack extends Character implements Serializable {
 
     public boolean collided;
 
-    public Attack(Vector2 pos, String spriteLink, Player player) {
+    public Attack(Vector2 pos, String spriteLink, Character c) {
 
         super(pos, spriteLink);
 
-        if (player.flipped){
+
+        if (c.flipped){
             sprite.flip(true,false);
             sprite.setX(pos.x-sprite.getWidth());
             direction.x = -1;
         } else {
-            sprite.setX(pos.x+player.sprite.getWidth()/2);
+            sprite.setX(pos.x+c.sprite.getWidth()/2);
             direction.x = 1;
         }
-        sprite.setRegion(0,0,25,25);
-        sprite.setScale(25/64f);
-        hitRect = new Rectangle(sprite.getX(),sprite.getY(),25*playerScale,25*playerScale);
+        sprite.setRegion(0,0,16,16);
+        sprite.setScale(1,1);
+
+        width = sprite.getWidth()*playerScale;
+        height = sprite.getHeight()*playerScale;
+
+        hitRect = new Rectangle(c.getMidX()-width/2,c.getMidY()-height/2,width,height);
 
         gravSpeed = 0;
         damage = 1;
--- core/src/com/mygdx/game/entities/Pegpeg.java
+++ core/src/com/mygdx/game/entities/Pegpeg.java
@@ -6,10 +6,15 @@ import com.mygdx.game.GameScreen;
 import com.mygdx.game.entities.loot.LootHealth;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Pegpeg extends Enemy implements Serializable {
+
+    ArrayList<Attack> attacks;
+
     public Pegpeg(Vector2 pos) {
         super(pos);
+        attacks = new ArrayList<>();
         width = 32;
         height = 32;
         sprite.setScale(.5f,1);
@@ -26,5 +31,29 @@ public class Pegpeg extends Enemy implements Serializable {
     public void dropLoot() {
         super.dropLoot();
         GameScreen.loot.add(new LootHealth(new Vector2(getMidX(),getMidY())));
+    }
+
+    //the enemy should shoot at the player when in range
+    @Override
+    void searchPlayer() {
+        for (Player p: GameScreen.players){
+            float dist = GameScreen.getDistance(p.getMidPosition(), this.getMidPosition());
+            if(dist < range){
+                //a function to determine which direction the enemy should head in
+                determineDirection(p);
+                if (dist < range/2f){
+                    shoot(p);
+                }
+            } else{
+                //set enemy direction to 0 if player is too far away
+                direction.x = 0;
+            }
+        }
+    }
+
+    //a function to make the enemy shoot
+    void shoot(Player player) {
+//        attacks.add(new Attack(getPosition(),"enemy/bullet.png", this));
+//        System.out.println(attacks);
     }
 }
