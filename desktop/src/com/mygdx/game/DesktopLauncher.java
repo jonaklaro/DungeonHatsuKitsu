2451a460f9d37ee7ee9551d08936cbaeff964e5b
jonanarchy <jmallet0507@gmail.com>
2022-06-22 16:14:33 +0200

    resizable change

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -7,15 +7,17 @@ import java.awt.*;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-  public static void main(String[] arg) {
-    Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-    config.setTitle("Acrobat Dungeon");
-    float windowScaleFactor = 0.7f;
-    int x = (int) (Toolkit.getDefaultToolkit().getScreenSize().width * windowScaleFactor);
-    int y = (int) (Toolkit.getDefaultToolkit().getScreenSize().height * windowScaleFactor);
-    config.setWindowedMode(x, y);
-    config.setWindowPosition(Toolkit.getDefaultToolkit().getScreenSize().width/2 - x/2, Toolkit.getDefaultToolkit().getScreenSize().height/2 - y/2);
-    config.setForegroundFPS(60);
-    new Lwjgl3Application(new AcrobatDungeon(), config);
-  }
+      public static void main(String[] arg) {
+            Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+            config.setTitle("Acrobat Dungeon");
+            float windowScaleFactor = 0.7f;
+            int x = (int) (Toolkit.getDefaultToolkit().getScreenSize().width * windowScaleFactor);
+            int y = (int) (Toolkit.getDefaultToolkit().getScreenSize().height * windowScaleFactor);
+            config.setWindowedMode(x, y);
+            config.setWindowPosition(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - x / 2,
+                        Toolkit.getDefaultToolkit().getScreenSize().height / 2 - y / 2);
+            config.setResizable(false);
+            config.setForegroundFPS(60);
+            new Lwjgl3Application(new AcrobatDungeon(), config);
+      }
 }
